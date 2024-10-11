88ecedd0b5faf462b96bb55430203f96a6599a34
yoshiultras <nikita.salivonov@yandex.ru>
2024-10-12 01:29:48 +0300

    v3 Соединение по нескольким значениям в одной записи

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-# -*- coding: utf-8 -*-
-
 import pandas as pd
-import locale
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
 
-locale.setlocale(locale.LC_ALL, 'ru_RU.UTF8')
+
+# Функция для обработки значений
+def process_data(series):
+    return series.apply(
+        lambda x: x.strip().lower() if isinstance(x, str) else str(x).strip().lower()).apply(
+        lambda x: [num.strip() for num in x.split(';') if num.strip() != '+7' and '_' not in num]
+        if isinstance(x, str) else [])
 
 
-class CSVMergerApp:
+def merge_excel(df1, df2, common_field1, common_field2):
+    if common_field1 != 'ФИО':
+        df1[common_field1] = process_data(df1[common_field1])
+    if common_field2 != 'ФИО':
+        df2[common_field2] = process_data(df2[common_field2])
+
+    df1 = df1.explode(common_field1)
+    df2 = df2.explode(common_field2)
+
+    # Обработка пустых данных
+    # Следует переделать
+    df1[common_field1] = df1[common_field1].apply(lambda x: ' ' if x == [] or x == 'nan' or str(x) is None else x)
+    df1.fillna(' ', inplace=True)
+
+    merged_df = pd.merge(df1, df2, left_on=common_field1, right_on=common_field2)
+    return merged_df
+
+
+class ExcelMergerApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Excel Merger")
@@ -17,8 +38,6 @@ class CSVMergerApp:
         self.file2 = ""
         self.columns_file1 = []
         self.columns_file2 = []
-        self.data_flow1 = None
-        self.data_flow2 = None
 
         # Кнопка для загрузки первого файла
         self.btn_load_file1 = tk.Button(root, text="Загрузить первый файл", command=self.load_file1)
@@ -44,7 +63,7 @@ class CSVMergerApp:
         self.combobox_file2.pack(pady=5)
 
         # Кнопка для выполнения объединения
-        self.btn_merge = tk.Button(root, text="Объединить файлы", command=self.merge_csv)
+        self.btn_merge = tk.Button(root, text="Объединить файлы", command=self.merge)
         self.btn_merge.pack(pady=20)
 
     def load_file1(self):
@@ -52,8 +71,8 @@ class CSVMergerApp:
         if self.file1:
             print(f"Выбран первый файл: {self.file1}")
             self.file1_label.config(text=self.file1)
-            self.data_flow1 = pd.read_excel(self.file1)
-            self.columns_file1 = self.data_flow1.columns.tolist()
+            df1 = pd.read_excel(self.file1)
+            self.columns_file1 = df1.columns.tolist()
             self.combobox_file1['values'] = self.columns_file1
             self.combobox_file1.set('')  # Сброс выбора
 
@@ -62,12 +81,12 @@ class CSVMergerApp:
         if self.file2:
             print(f"Выбран второй файл: {self.file2}")
             self.file2_label.config(text=self.file2)
-            self.data_flow2 = pd.read_excel(self.file2)
-            self.columns_file2 = self.data_flow2.columns.tolist()
+            df2 = pd.read_excel(self.file2)
+            self.columns_file2 = df2.columns.tolist()
             self.combobox_file2['values'] = self.columns_file2
             self.combobox_file2.set('')  # Сброс выбора
 
-    def merge_csv(self):
+    def merge(self):
         common_field1 = self.combobox_file1.get().strip()
         common_field2 = self.combobox_file2.get().strip()
 
@@ -80,14 +99,25 @@ class CSVMergerApp:
             return
 
         try:
-            # Объединение по выбранным столбцам
-            merged_df = pd.merge(self.data_flow1, self.data_flow2, left_on=common_field1, right_on=common_field2)
+            df1 = pd.read_excel(self.file1)
+            df2 = pd.read_excel(self.file2)
+            merged_df = merge_excel(df1, df2, common_field1, common_field2)
 
-            # Сохранение результата в новый CSV файл
+            # Код отладки
+            # pd.set_option('display.max_rows', None)
+            # pd.set_option('display.max_columns', None)
+            # pd.set_option('display.width', None)
+            # pd.set_option('display.max_colwidth', None)
+            # print(merged_df)
+
+            # Сохранение результата в новый файл
             merged_df.to_excel("merged_output.xlsx", index=False)
             messagebox.showinfo("Успех", "Файлы успешно объединены в 'merged_output.xlsx'.")
+
         except KeyError as e:
             messagebox.showerror("Ошибка", f"Столбец '{e}' не найден в одном из файлов.")
+        except PermissionError as e:
+            messagebox.showerror("Ошибка", f"К файлу '{e.filename}' запрещен доступ. Закройте файл или запустите программу от имени администратора.")
         except Exception as e:
             messagebox.showerror("Ошибка", str(e))
 
@@ -95,5 +125,5 @@ class CSVMergerApp:
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry('520x420')
-    app = CSVMergerApp(root)
+    app = ExcelMergerApp(root)
     root.mainloop()
